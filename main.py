24e2f013c14bdeda2b414f564b7d6eff135b1bbc
davidtc8 <davidtorresc8@gmail.com>
2021-02-12 17:17:14 -0600

    Password Manager Code.

---
+++ main.py
@@ -0,0 +1,11 @@
+from tkinter import *
+# ---------------------------- PASSWORD GENERATOR ------------------------------- #
+
+# ---------------------------- SAVE PASSWORD ------------------------------- #
+
+# ---------------------------- UI SETUP ------------------------------- #
+window = Tk()
+window.title("Password Manager")
+
+
+window.mainloop()
